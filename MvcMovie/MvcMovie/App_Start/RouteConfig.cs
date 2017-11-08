6d986511e2c3c231349cc4b1acf485bd0cd85e7d
miyaluc <srlucas3@gmail.com>
2017-11-08 09:52:39 -0800

    set the format for routing

--- MvcMovie/MvcMovie/App_Start/RouteConfig.cs
+++ MvcMovie/MvcMovie/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace MvcMovie
                     action = "Index",
                     id = UrlParameter.Optional
                 }
-            );
+                    );
         }
     }
 }
