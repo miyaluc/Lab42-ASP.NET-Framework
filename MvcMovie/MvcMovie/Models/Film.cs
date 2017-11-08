62adf8b171d2394968897f7e29a8720e28662872
miyaluc <srlucas3@gmail.com>
2017-11-08 14:15:09 -0800

    added validation logic to the film model

--- MvcMovie/MvcMovie/Models/Film.cs
+++ MvcMovie/MvcMovie/Models/Film.cs
@@ -13,14 +13,20 @@ namespace MvcMovie.Models
         {
             get; set;
         }
+        [StringLength(60, MinimumLength = 3)]
         public string Title
         {
             get; set;
         }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre
         {
             get; set;
         }
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price
         {
             get; set;
@@ -32,6 +38,8 @@ namespace MvcMovie.Models
         {
             get; set;
         }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(5)]
         public string Rating
         {
             get; set;
