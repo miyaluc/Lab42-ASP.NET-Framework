b5fcc96796acfb08ff21dbf9d2e2765adfe2250c
miyaluc <srlucas3@gmail.com>
2017-11-08 09:48:13 -0800

    added first controller with actions

---
+++ MvcMovie/MvcMovie/Controllers/GoodbyeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class GoodbyeController : Controller
+    {
+        // GET: /Goodbye/
+        public string Index()
+        {
+            return "Here is my default action.";
+        }
+
+        // GET: /Goodbye/Aloha/ 
+
+        public string Aloha()
+        {
+            return "Aloha means hello and goodbye....";
+        }
+    }
+}
